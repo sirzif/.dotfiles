57d60ad99cc9eb34d143cb778f72cd919830370a
sirzif <99730587+sirzif@users.noreply.github.com>
2025-12-26 18:32:19 +0200

    bruh

--- env/.config/nvim/lua/martynasmuizys/lazy/local.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"horizn",
-	dir = "~/dev/horizn/",
-	dependencies = { "nvim-tree/nvim-web-devicons", "tpope/vim-fugitive" },
-	config = function()
-		require("horizn").setup({})
-	end,
-}
